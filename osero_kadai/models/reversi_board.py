84bb519046ed9f2891757de13cc5a9fbda7e46ea
Asahi Yamada <soccer.as.ywgoogl@gmail.com>
2023-08-08 18:01:23 +0900

    関数名変更

--- osero_kadai/models/reversi_board.py
+++ osero_kadai/models/reversi_board.py
@@ -4,11 +4,13 @@ import random
 
 class ReversiBoard():
     def __init__(self):
+       
         self.board=np.zeros((9,9))
         self.board[4,4]= 1
         self.board[5,5]= 1
         self.board[4,5]= -1
         self.board[5,4]= -1
+
         self.available_list=[]
         self.i_show_board_splitting_function=0
 
@@ -16,7 +18,7 @@ class ReversiBoard():
 
     def show_board(self, color=1):
         self._make_baseboard
-        self._put_all
+        self._put__
 
         plt.xlim(0, 720)
         plt.ylim(0, 720)
@@ -30,18 +32,17 @@ class ReversiBoard():
         _, self.ax=plt.subplots()
         for y in range(9):
             for x in range(9):
-                self.show(x, y)
-
-    def show(self, x, y):
+                self.write_on_board(x, y)
+            
+    def write_on_board(self, x, y):
         y_=y*80
         x_=x*80
         if (x==0)|(y==8):
-            self._show_number(x_, y_)
+            self._write_number(x_, y_)
         else:
-            self._show_point(x_, y_)
+            self._write_point(x_, y_)
 
-
-    def _show_number(self, x_, y_):
+    def _write_number(self, x_, y_):
         if self.i_show_board_splitting_function== 8:
             point=None
             self.i_show_board_splitting_function+= 1
@@ -50,18 +51,18 @@ class ReversiBoard():
             self.i_show_board_splitting_function+= 1
         self.ax.text(x_+40, y_+40, point, ha="center", va="center", fontsize=10)
 
-    def _show_point(self, x_, y_):
+    def _write_point(self, x_, y_):
         rectangle = plt.Rectangle((x_, y_), 80, 80, edgecolor="black", facecolor="green")
         self.ax.add_patch(rectangle)
 
     @property
-    def _put_all(self):
+    def _put_points(self):
         for x in range(9):
             for y in range(9):
-                self.put_coin(x, y)
+                self.put_stone(x, y)
                 self.put_available_point(x, y)
 
-    def put_coin(self, x, y):
+    def put_stone(self, x, y):
         if int(self.board[x][y])== 1:
             circle = plt.Circle((x*80+40, 680-y*80), 30, edgecolor= "black", facecolor= "black")
             self.ax.add_patch(circle)
@@ -81,25 +82,6 @@ class ReversiBoard():
         self.update_diagonal1(x, y, color)
         self.update_diagonal2(x, y, color)
 
-    def check_x(self,x,z,y,color):
-
-        if self.board[z,y]== color:
-                self.board[z:x,y]= color
-                return True
-        elif self.board[z,y]== 0:
-                return True
-        else:
-                return False
-    def check_y(self,x,z,y,color):
-
-        if self.board[x,z]== color:
-                self.board[x,y:z]= color
-                return True
-        elif self.board[x,z]== 0:
-                return True
-        else:
-                return False   
-
     def update_length(self, x, y, color):
         for i in range(1,x):
             if self.board[x-i,y]== color:
@@ -108,7 +90,7 @@ class ReversiBoard():
             elif self.board[x-i,y]== 0:
                 break
             else:
-                continue        
+                continue
 
         for i in range(1,9-x):
             if self.board[x+i,y]== color:
@@ -164,6 +146,7 @@ class ReversiBoard():
                 continue
 
     def update_diagonal2(self, x, y, color):
+
         for i in range(1, min(9-x,y)+1):
             if (x+i > 8)|(y-i < 1):
                 break
